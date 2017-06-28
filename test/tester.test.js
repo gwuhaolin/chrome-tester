576e12e46e98a3930a6aadb603a0b33f8a210917
halwu(吴浩麟) <halwu@tencent.com>
2017-06-28 11:52:04 +0800

    travis add all os

--- test/tester.test.js
+++ test/tester.test.js
@@ -4,7 +4,7 @@ const Tester = require('../index');
 process.on('unhandledRejection', console.trace);
 
 describe('Tester', function () {
-  this.timeout(5000);
+  this.timeout(10000);
 
   it('basic', async function () {
     const tester = new Tester();
